e59075f905cdac5d71b224863c2f979143f00dc5
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2022-11-15 22:24:14 +0200

    add solution task-1

---
+++ src/js/02-timer.js
@@ -0,0 +1,20 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+
+const refs = {
+  inputText: document.querySelector('#datetime-picker'),
+};
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr(refs.inputText, options);
